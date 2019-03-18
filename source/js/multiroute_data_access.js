104d0e4d559f946d0f8bc796ae72c2e3291c5ecf
Asgen <genuus1@gmail.com>
2019-03-18 17:51:07 +0300

    Построение маршрута

--- source/js/multiroute_data_access.js
+++ source/js/multiroute_data_access.js
@@ -1,144 +1,192 @@
 var KeyCode = {
-	ESC_KEYCODE: 27,
-	ENTER_KEYCODE: 13
+  ESC_KEYCODE: 27,
+  ENTER_KEYCODE: 13
 };
 
-var points = ["Анапа"];
+var editedPoints;
+var input = document.querySelector('.data__input');
+var pointList = document.querySelector('.data__list');
+var points = [];
 
-var xxx = ["Казань","Ростов"];
-
-var defaultLoc1 = "Спб";
-var defaultLoc2 = "Анадырь";
-
-ymaps.ready(init(defaultLoc1, defaultLoc2));
-
-function init (param1) {
-	return function () {
-		// Создаем модель мультимаршрута.
-		/*var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([
-				// Путевые точки
-				param1,
-				"Москва, ул. Мясницкая"
-			], {
-				// Путевые точки можно перетаскивать.
-				// Маршрут при этом будет перестраиваться.
-				wayPointDraggable: true,
-				boundsAutoApply: true
-			});
-
-		ymaps.modules.require([
-			'MultiRouteCustomView'
-		], function (MultiRouteCustomView) {
-			// Создаем экземпляр текстового отображения модели мультимаршрута.
-			// см. файл custom_view.js
-			new MultiRouteCustomView(multiRouteModel);
-		});*/
+var errorMessage = "Ой, кажется такого нет на Земле. Попробуйте ввести по-другому.";
 
 
 
+// Удаление путевой точки
+var onCloseBtnClick = function (evt, mRoute, map) {
+  // Получаем удаляемую точку
+  var destToRemove = evt.target.parentElement.textContent;
+  if (destToRemove !== errorMessage) {
+    console.log(editedPoints);
+    var indexToRemove = editedPoints.indexOf(destToRemove);
+    points.splice(indexToRemove, 1);
+    editedPoints.splice(indexToRemove, 1);
+    evt.target.parentElement.remove();
+
+    console.log(destToRemove);
+    console.log(points);
+
+    //updateRoute(mRoute, points, map);
+    mRoute.model.setReferencePoints(points, []);
+  }
+  else {
+    evt.target.parentElement.remove();
+    console.log(editedPoints);
+  }
+
+  // Центровка карты после перестроения маршрута
+    //updateRoute(mRoute, points, map);
+
+
+  /*// Добавление введенного местоположения в массив путевых точек
+  points.push(newPoint);
+  // Очищаем поле ввода
+  input.value = "";
+
+  // Добавляем точку во фронтенд
+  // Редактирование введенного пользователем местоположения
+  mRoute.events
+  .add("update", function () {
+    var wayPoints = mRoute.getWayPoints();
+    // Проход по коллекции путевых точек.
+    // Для каждой точки зададим содержимое меток.
+    wayPoints.each(function (point, index) {
+      //console.log(point.properties._data.address);
+      if (index === 0) {
+        editedPoints = [];
+      }
+      editedPoints.push(point.properties._data.address);
+      updatePoints(editedPoints);
+    });
+  });
+
+  // Обновление маршрута
+  mRoute.model.seReferencePoints(points, []);
+
+  // Центровка карты после перестроения маршрута
+  mRoute.events
+  .add("boundschange", function () {
+    // Центровка карты по маршруту
+    map.setBounds(mRoute.getBounds());
+
+    // Изменение масштаба карты, когда задана всего одна путевая точка
+    if(points.length < 2) {
+      map.setZoom( 10 );
+    }
+  });*/
+};
+
+// Добавление путевой точки
+var onKeydownInput = function (evt, mRoute, map) {
+
+  if (evt.keyCode === KeyCode.ENTER_KEYCODE) {
+    // Получение координат введенного пользователем местоположения.
+    var newPoint = input.value;
+    // Добавление введенного местоположения в массив путевых точек
+    points.push(newPoint);
+    // Очищаем поле ввода
+    input.value = "";
+
+    // Добавляем точку во фронтенд
+    // Редактирование введенного пользователем местоположения
+    mRoute.events
+    .add("update", function () {
+      var wayPoints = mRoute.getWayPoints();
+      // Проход по коллекции путевых точек.
+      // Для каждой точки зададим содержимое меток.
+      wayPoints.each(function (point, index) {
+        if (index === 0) {
+          editedPoints = [];
+        }
+        editedPoints.push(point.properties._data.address);
+        updatePoints(editedPoints, mRoute);
+      });
+    });
+
+    // Перерисовка маршрута с центровкой карты
+    updateRoute(mRoute, points, map);
+  }
+};
 
 
 
-		// Объявляем набор опорных точек и массив индексов транзитных точек.
-	    var referencePoints = [
-	            "Москва, Ленинский проспект",
-	            "Москва, Льва Толстого, 16",
-	            "Москва, Кремлевская набережная",
-	            "Москва, парк Сокольники"
-	        ];
+// Обновление маршрута
+var updateRoute = function (mRoute, pointsArr, map) {
+  // Обновление маршрута
+  mRoute.model.setReferencePoints(pointsArr, []);
 
-	    // Создаем мультимаршрут и настраиваем его внешний вид с помощью опций.
-	    var multiRoute = new ymaps.multiRouter.MultiRoute({
-	        referencePoints: referencePoints
-	    }, {
-	        
+  // Центровка карты после перестроения маршрута
+  mRoute.events
+  .add("update", function () {
+    // Центровка карты по маршруту
+    map.setBounds(mRoute.getBounds());
 
-	        
+    // Изменение масштаба карты, когда задана всего одна путевая точка
+    if(pointsArr.length < 2) {
+      map.setZoom( 16 );
+    }
+  });
+};
 
-	        // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
-	        boundsAutoApply: true,
-	        wayPointDraggable: true
-	    });
+// Обновление путевых точек (frontend)
+var updatePoints = function (newPointsArr, mRoute) {
+  // Чистка списка путевых точек
+  while (pointList.firstChild) {
+    pointList.removeChild(pointList.firstChild);
+  }
+  // Перерисовка списка путевых точек
+  for (var i = 0; i < newPointsArr.length; i++) {
 
+    if (newPointsArr[i] === undefined) {
+      newPointsArr[i] = errorMessage;
 
+      var wayPoints = mRoute.getWayPoints();
+      // Проход по коллекции путевых точек.
+      // Для каждой точки зададим содержимое меток.
+      wayPoints.each(function (point, index) {
+        if (point.properties._data.coordinates && point.properties._data.coordinates.length > 0 && index === i) {
+          newPointsArr[i] = point.properties._data.coordinates[0].toFixed(3) + ', ' + point.properties._data.coordinates[1].toFixed(3)
+        }
+      });
+    }
 
+    let listElement = document.createElement('li');
+    let btn = document.createElement("BUTTON");
+    listElement.classList.add('data__point');
+    btn.classList.add('data__close-btn');
+    btn.addEventListener('click', function (evt) {onCloseBtnClick(evt, mRoute)});
+    listElement.textContent = newPointsArr[i];
+    listElement.appendChild(btn);
+    pointList.appendChild(listElement);
+  }
+};
 
+ymaps.ready(init);
 
+function init () {
+  // Создаем мультимаршрут и настраиваем его внешний вид с помощью опций.
+  var multiRoute = new ymaps.multiRouter.MultiRoute({
+      referencePoints: points
+  }, {
 
+      // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+      boundsAutoApply: true,
+      wayPointDraggable: true
+  });
 
-		// Создаем карту.
-		var mapCont = document.querySelector('#map'),
-			myMap = new ymaps.Map(mapCont, {
-				// Центр карты
-				center: [35.750625, 37.626],
-				zoom: 7
-			}, {
-				buttonMaxWidth: 300
-			});
+  // Создаем карту.
+  var mapCont = document.querySelector('#map'),
+    myMap = new ymaps.Map(mapCont, {
+      // Центр карты
+      center: [55.739625, 37.54120],
+      zoom: 4
+    }, {
+      buttonMaxWidth: 300
+    });
 
-			/*// Создаем на основе существующей модели мультимаршрут.
-			multiRoute = new ymaps.multiRouter.MultiRoute(multiRouteModel, {
-				// Путевые точки можно перетаскивать.
-				// Маршрут при этом будет перестраиваться.
-				wayPointDraggable: true,
-				boundsAutoApply: true
-			});*/
+  // Добавление путевой точки
+  input.addEventListener('keydown', function (evt) {onKeydownInput(evt, multiRoute, myMap)});
 
-			// Добавление путевой точки
-			var input = document.querySelector('.data__input');
-			input.addEventListener('keydown', function (evt) {
-				if (evt.keyCode === KeyCode.ENTER_KEYCODE) {
-					// Получение координат введенного пользователем местоположения.
-					var newPoint = input.value;
-					points.push(newPoint);
-					console.log(points);
-
-
-
-
-
-					// Создаем мультимаршрут и настраиваем его внешний вид с помощью опций.
-				    MultiRoute = new ymaps.multiRouter.MultiRoute({
-				        referencePoints: xxx
-				    }, {
-				        
-
-				        
-
-				        // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
-				        wayPointDraggable: true,
-				        boundsAutoApply: true
-				    });
-
-
-
-// Добавляем мультимаршрут на карту.
-		//multiRoute.destroy();
-		myMap.geoObjects.add(multiRoute);
-
-
-
-
-					/*// Добавление путевой точки в маршрут
-					multiRoute.model.setReferencePoints([
-				      points[0],
-				      points[points.length-1]
-				    ], []);*/
-
-					// Центровка карты после перестроения маршрута
-					multiRoute.events
-					.add("boundschange", function () {
-				    myMap.setBounds(multiRoute.getBounds());
-				    console.log(multiRoute.model.getReferencePoints());
-					});
-				}
-
-			});
-
-		// Добавляем мультимаршрут на карту.
-		myMap.geoObjects.add(multiRoute);
-	};
-}
-
-window.init = init;
-
+  // Добавляем мультимаршрут на карту.
+  myMap.geoObjects.add(multiRoute);
+};
